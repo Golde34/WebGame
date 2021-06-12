f77336061c141c3027fe6522c8ab3e9c065fcca9
duongyk <85751059+duongyk@users.noreply.github.com>
2021-06-12 23:52:22 +0700

    Update Order.java

--- WebGame/src/java/entity/Order.java
+++ WebGame/src/java/entity/Order.java
@@ -7,8 +7,65 @@ package entity;
 
 /**
  *
- * @author Admin
+ * @author Duong
  */
 public class Order {
+    private int oId;
+    private int uId;
+    private double total;
+    private int status;
+
+    public Order() {
+    }
+
+    public Order(int oId, int uId, double total, int status) {
+        this.oId = oId;
+        this.uId = uId;
+        this.total = total;
+        this.status = status;
+    }
+
+    public Order(int oId, int uId, double total) {
+        this.oId = oId;
+        this.uId = uId;
+        this.total = total;
+    }
+
+    public int getoId() {
+        return oId;
+    }
+
+    public void setoId(int oId) {
+        this.oId = oId;
+    }
+
+    public int getuId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "oId=" + oId + ", uId=" + uId + ", total=" + total + ", status=" + status + '}';
+    }
     
 }
