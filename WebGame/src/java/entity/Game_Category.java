dd630f516d390ea1de3a6551a8464ed192fbc024
DuongNH8837 <85751304+DuongNH8837@users.noreply.github.com>
2021-06-13 12:23:52 +0700

    Update Game_Category.java
    updated

--- WebGame/src/java/entity/Game_Category.java
+++ WebGame/src/java/entity/Game_Category.java
@@ -10,5 +10,40 @@ package entity;
  * @author Admin
  */
 public class Game_Category {
-    
+    private int caId;
+    private int gId;
+    private boolean status;
+
+    public Game_Category() {
+    }
+
+    public Game_Category(int caId, int gId, boolean status) {
+        this.caId = caId;
+        this.gId = gId;
+        this.status = status;
+    }
+
+    public int getCaId() {
+        return caId;
+    }
+
+    public int getgId() {
+        return gId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setCaId(int caId) {
+        this.caId = caId;
+    }
+
+    public void setgId(int gId) {
+        this.gId = gId;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
