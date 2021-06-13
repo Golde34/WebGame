a8018aeae9c7177073f4a2160ed14d50ce1c0bfa
DuongNH8837 <85751304+DuongNH8837@users.noreply.github.com>
2021-06-13 12:25:49 +0700

    Update Category.java
    updateed

--- WebGame/src/java/entity/Category.java
+++ WebGame/src/java/entity/Category.java
@@ -10,5 +10,50 @@ package entity;
  * @author Admin
  */
 public class Category {
-    
+    private int caId;
+    private String caName;
+    private String descript;
+    private boolean status;
+
+    public Category() {
+    }
+
+    public Category(int caId, String caName, String descript, boolean status) {
+        this.caId = caId;
+        this.caName = caName;
+        this.descript = descript;
+        this.status = status;
+    }
+
+    public int getCaId() {
+        return caId;
+    }
+
+    public String getCaName() {
+        return caName;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setCaId(int caId) {
+        this.caId = caId;
+    }
+
+    public void setCaName(String caName) {
+        this.caName = caName;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
