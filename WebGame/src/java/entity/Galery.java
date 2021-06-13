b671de04be96bda58bb3efd7898e127ce153bac1
DuongNH8837 <85751304+DuongNH8837@users.noreply.github.com>
2021-06-13 12:24:41 +0700

    Update Galery.java
    updated

--- WebGame/src/java/entity/Galery.java
+++ WebGame/src/java/entity/Galery.java
@@ -10,5 +10,50 @@ package entity;
  * @author Admin
  */
 public class Galery {
-    
+    private int gId;
+    private String link;
+    private String type;
+    private boolean status;
+
+    public Galery() {
+    }
+
+    public Galery(int gId, String link, String type, boolean status) {
+        this.gId = gId;
+        this.link = link;
+        this.type = type;
+        this.status = status;
+    }
+
+    public int getgId() {
+        return gId;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setgId(int gId) {
+        this.gId = gId;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
